72f9971f335a335329f248df62ed0c429fa1fd6d
DSS93 <36404391+DSS93@users.noreply.github.com>
2024-12-24 14:45:49 +0530

    Add files via upload

---
+++ others/deletefuncslot.ps1
@@ -0,0 +1,32 @@
+﻿# Variables
+$ResourceGroupName = "rg-runbook"
+
+# Authenticate to Azure
+Connect-AzAccount
+
+# Get all App Services in the specified App Service Plan
+$appServices = Get-AzFunctionApp -ResourceGroupName "rg-runbook" | Where-Object { $_.Name -like "func-*" -and $_.Name -notlike "func-dss-001*" }
+
+# Calculate the date 24 hours ago
+$thresholdDate = (Get-Date).AddHours(-24)
+ 
+
+# Loop through each App Service
+foreach ($appService in $appServices) {
+    # Get all slots for the App Service
+    $slots = Get-AzWebAppSlot -ResourceGroupName $resourceGroupName -Name $appService.Name | Where-Object { $_.LastModifiedTimeUtc -lt $thresholdDate }
+    if ($slots -eq $null) {
+        Write-Output "No slots created more than 24 hours ago."
+    } else {
+        foreach ($slot in $slots) {
+            $splitString = $slot.Name -split '/'
+            $slotName = $splitString[1]
+            Remove-AzWebAppSlot -ResourceGroupName $resourceGroupName -Name $appService.Name -Slot $slotName -Force
+            Write-Output "Deleted slot: $($slot.Name)"
+        }
+    }
+
+}
+
+
+
